b51ec1aa6f19e1fb019073fe29ec5f8923ce0325
Roman <r_af_95@hotmail.com>
2021-05-06 17:46:49 -0300

    Cambios

---
+++ clase_2/EJ1.9.js
@@ -0,0 +1,15 @@
+var a=1;
+var b=2;
+var c=3;
+var d=a+b+c;
+var a=b;
+var b=3;
+
+console.log(a);
+
+var nombre= "Roman";
+var edad=26;
+var cumple= 04/04/1995;
+var ciudad= "Buenos Aires"
+var ocupacion= "Estudiante"
+console.log("Hola " + nombre + " de " + edad + " años" + ", nacido el " + cumple + " en " + ciudad + " de ocupación " + ocupacion)
